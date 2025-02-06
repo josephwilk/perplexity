411063ba5d114a153375ea02bd02e201db9dc7e7
Joseph Wilk <joe@josephwilk.net>
2025-02-06 17:46:45 +0000

    Uses threads

--- score.py
+++ score.py
@@ -1,20 +1,25 @@
 import sys
-
-if(len(sys.argv) < 2):
-    print("Usage: python score.py FILENAME")
-    exit(1)
-    
-target = sys.argv[1]
+import glob, re
 
 import finewebed
 import perplexity
 
-with open(target) as f:
-    text = f.read()
-    p = perplexity.perplexity(text)
-    f = finewebed.finewebed(text)
+from concurrent.futures import ThreadPoolExecutor
 
-    print(f"{target}: Perplexity: {p} | Quality: {perplexity.category(p)}")
-    print(f"{target}: FineWebEd:  {f} | {finewebed.category(f)}")
+if(len(sys.argv) < 2):
+    print("Usage: python score.py DIRNAME")
+    exit(1)
+    
+target = sys.argv[1]
+DATA_FILES = glob.glob(target+'**/*.txt', recursive=True)
 
+def print_score(filename):
+    with open(filename) as f:
+        text = f.read()
+        p = perplexity.perplexity(text)
+        f = finewebed.finewebed(text)
+        print(f"{filename}: Perplexity: {p} | Quality: {perplexity.category(p)}")
+        print(f"{filename}: FineWebEd:  {f} | {finewebed.category(f)}")
+        print("")
 
+executor = ThreadPoolExecutor(20).map(print_score, DATA_FILES)
